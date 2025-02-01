9fa89764b5fd37fe19575f40216b58a657094d27
SamySbh <samy.sebahi@yahoo.fr>
2025-02-01 14:31:58 +0100

    Add prisma config

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import express from 'express';
 
+const app = express()
+const port = process.env.PORT || 3000
+
+// Middleware to serve static files from the 'public' directory.
 app.use(express.static('public'))
 
+// Middleware to parse JSON request bodies.
+app.use(express.json());
+
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
+})
+
+
+app.get('/samy', (req, res) => {
+    res.send('Hello World ! Im Samy kkkk dd$$$')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
